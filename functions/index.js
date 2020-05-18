224b37fb51dd424666f03d0c67c3f07d2fef52a6
Alvin <ad.gregory@uniandes.edu.co>
2020-05-18 03:30:18 -0500

    La marimonda

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,46 @@
-// const functions = require('firebase-functions');
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+'use strict';
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  console.log('Hello')
-//  response.send("Hello from Firebase!");
-// });
 
-const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+// FireBase
+const functions = require('firebase-functions');
+const Storage = require('@google-cloud/storage');
+const os = require('os');
+const path = require('path');
+//const gcs = Storage();
+
+
+// IBM API
+const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
 const fs = require('fs');
 const { IamAuthenticator } = require('ibm-watson/auth');
 const parameters = require("./parameters.json");
 
-const VisualRecognition = new VisualRecognitionV3({
-        version: '2018-03-19',
-        authenticator: new IamAuthenticator({apikey:parameters.VR_API_KEY}),
-        url: parameters.VR_URL
-});
+
+// const VisualRecognition = new VisualRecognitionV3({
+//     version: '2018-03-19',
+//     authenticator: new IamAuthenticator({apikey:parameters.VR_API_KEY}),
+//     url: parameters.VR_URL
+// });
 
 
-const classifyParams = {
-    imagesFile: fs.createReadStream('./images_Test/cooking.jpg'),
-    classifierIds: parameters.VR_MODEL_ID,
-    threshold: 0.6,
-};
+// const classifyParams = {
+//     imagesFile: fs.createReadStream('./images_Test/cooking.jpg'),
+//     classifierIds: parameters.VR_MODEL_ID,
+//     threshold: 0.6,
+// };
 
-VisualRecognition.classify(classifyParams, function(err,response){
-    if (err){
-        console.log(err);
+// VisualRecognition.classify(classifyParams, function(err,response){
+//     if (err){
+//         console.log(err);
+//     } else{
+//         console.log(JSON.stringify(response,null,2))
+//     }
+// });
+
+exports.imageChecking = functions.storage.object().onFinalize((event) => {
+    if(!event.name.startsWith('cover_streamings/')){
+        return console.log(event.name); 
     } else{
-        console.log(JSON.stringify(response,null,2))
-    }
+        return null
+    }  
 });
