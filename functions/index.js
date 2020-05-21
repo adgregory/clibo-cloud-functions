297455a53ee613dcebe7f4cc1983e9d6f98f3afd
Alvin <ad.gregory@uniandes.edu.co>
2020-05-20 21:10:13 -0500

    Visual Recognition3

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,12 @@
 
 const https = require('https');
 const Stream = require('stream').Transform;
+const functions = require("firebase-functions");
+// const gcs = require('@google-cloud/storage');
+const path = require('path');
+
+const {Storage} = require('@google-cloud/storage'); // Creates a client const storage = new Storage();
+const storage = new Storage();
 // IBM API
 const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
 const NaturalLanguageCLassifierV1 = require('ibm-watson/natural-language-classifier/v1');
@@ -34,126 +40,143 @@ firebaseAdmin.initializeApp({
     storageBucket: 'ibm-challenge-d1eaa.appspot.com'
 });
 
-const bucket = async () => {
-    let url = '';
-    // FOLDER COVER STREAMINGS --> VR_EXPLICIT, VR_GENERAL
-    const load = firebaseAdmin.storage().bucket();
-    load.getFiles(async (error, files) => {
-        url = await  files[files.length-1].getSignedUrl({
-            action: 'read',
-            expires: '09-20-2021'
-        });
-    console.log(files[files.length-1].name)        
-    // if (files[10].name.slice(0,7)=="profile"){ //files.length-1
-    //     let image = new Stream();
-    //     https.get(url[0], response => {
-    //         console.log(url[0])
-    //         response.on('data', chunk => {
-    //             image.push(chunk);
-    //         });
+// const bucket = async (imagen) => {
+//     let url = '';
+//     // FOLDER COVER STREAMINGS --> VR_EXPLICIT, VR_GENERAL
+//     //const load = firebaseAdmin.storage().bucket();
+//     console.log(imagen)
+//     load.getFiles(async (error, files) => { //REVISAR
+//         url = await  files[1].getSignedUrl({
+//             action: 'read',
+//             expires: '09-20-2021'
+//         });
+//     //console.log(url[0]) 
+//     if (files[0].name.slice(0,7)=="profile"){ //files.length-1
+//         console.log('Entro a Profile')
+//         let image = new Stream();
+//         https.get(url[0], response => {
+//             console.log(url[0])
+//             response.on('data', chunk => {
+//                 image.push(chunk);
+//             });
+//             response.on('end', () => {
+//                 //NEW (1)
+//                 const VisualClassifyParams_Explicit = {
+//                     imagesFile: image.read(),
+//                     classifierIds: parameters.VR_MODEL_ID_EXPLICIT,
+//                     threshold: 0.6,
+//                 };    
+//                 VisualRecognition.classify(VisualClassifyParams_Explicit, function (err, response) {
+//                     if (err) {
+//                         console.log(err);
+//                     } else {
+//                         const vr = JSON.stringify(response, null, 2)
+//                         const classes = response.result.images[0].classifiers[0].classes;
+//                         //console.log(classes)
+//                         if (classes[0] == 'explicit'){
+//                             console.log('0') //No aprobada
+//                         }else{
+//                             console.log('1') //Aprobada
+//                         }
+//                     }
+//                 });
+//             })
+//         });
+//     } else if(files[0].name.slice(0,5)=='cover'){//files.length-1
+//         console.log('Entro a Cover')
+//         let image = new Stream();
+//         https.get(url[0], response => {
+//             //console.log(url[0])
+//             response.on('data', chunk => {
+//                 image.push(chunk);
+//             });
+//             response.on('end', () => {
+//                 //console.log(image);
+//                 const VisualClassifyParams_General = {
+//                     imagesFile: image.read(),
+//                     classifierIds: parameters.VR_MODEL_ID,
+//                     threshold: 0.6,
+//                 };
+//                 //NEW (1)
+//                 const VisualClassifyParams_Explicit = {
+//                     imagesFile: image.read(),
+//                     classifierIds: parameters.VR_MODEL_ID_EXPLICIT,
+//                     threshold: 0.6,
+//                 };
     
-    //         response.on('end', () => {
-    //             //NEW (1)
-    //             const VisualClassifyParams_Explicit = {
-    //                 imagesFile: image.read(),
-    //                 classifierIds: parameters.VR_MODEL_ID_EXPLICIT,
-    //                 threshold: 0.6,
-    //             };    
-    //             VisualRecognition.classify(VisualClassifyParams_Explicit, function (err, response) {
-    //                 if (err) {
-    //                     console.log(err);
-    //                 } else {
-    //                     const vr = JSON.stringify(response, null, 2)
-    //                     const classes = response.result.images[0].classifiers[0].classes;
-    //                     //console.log(classes)
-    //                     if (classes[0] == 'explicit'){
-    //                         console.log('0') //No aprobada
-    //                     }else{
-    //                         console.log('1') //Aprobada
-    //                     }
-    //                 }
-    //             });
-    //         })
-    //     });
-    // } else if(files[10].slice(0,5)=='cover'){//files.length-1
-    //     let image = new Stream();
-    //     https.get(url[0], response => {
-    //         console.log(url[0])
-    //         response.on('data', chunk => {
-    //             image.push(chunk);
-    //         });
+//                 VisualRecognition.classify(VisualClassifyParams_Explicit, function (err, response) {
+//                     if (err) {
+//                         console.log(err);
+//                     } else {
+//                         const vr = JSON.stringify(response, null, 2)
+//                         const classes = response.result.images[0].classifiers[0].classes;
+//                         if (classes[0]=='explicit'){
+//                             console.log('0') //No aprobada
+//                         } else{
+//                             VisualRecognition.classify(VisualClassifyParams_General, function (err,response){
+//                                 if (err){
+//                                     console.log(err);
+//                                 } else {
+//                                     const vr2 = JSON.stringify(response, null, 2)
+//                                     const classes2 = response.result.images[0].classifiers[0].classes;
+//                                     console.log(classes2)
+//                                 };
+
+//                             });
+//                         }
+//                         //console.log(classes)
+//                     }
+//                 });
+//             })
+//         });
+//     } else if (files[files.length-1].slice(0,5)=='texto'){
+//         const classifyParamsText = {
+//             text: 'Me gusta la cuk',
+//             classifierId: parameters.LC_MODEL_ID
+//         };
+
+//         NaturalLanguageCLassifier.classify(classifyParamsText, function (err,response){
+//             if(err){
+//                 console.log(err);
+//             } else{
+//                 const lc = JSON.stringify(response, null, 2)
+//                 const classesLC = response.result.classes;
+//                 console.log(classesLC)
+//             };
+
+//         });
+//     };
+
+//      });
+//     //console.log(url);
+// }
+
+
+
+// exports.Classification = functions.storage.object().onFinalize(async event => {
+//     const bucket = gcs.Bucket(event.bucket);
+//     const path = event.name;
+//     const fileName = path.split('/').pop();
+//     const bucketDir = dirname(path);
     
-    //         response.on('end', () => {
-    //             //console.log(image);
-    //             const VisualClassifyParams_General = {
-    //                 imagesFile: image.read(),
-    //                 classifierIds: parameters.VR_MODEL_ID,
-    //                 threshold: 0.6,
-    //             };
-    //             //NEW (1)
-    //             const VisualClassifyParams_Explicit = {
-    //                 imagesFile: image.read(),
-    //                 classifierIds: parameters.VR_MODEL_ID_EXPLICIT,
-    //                 threshold: 0.6,
-    //             };
-    
-    //             VisualRecognition.classify(VisualClassifyParams_Explicit, function (err, response) {
-    //                 if (err) {
-    //                     console.log(err);
-    //                 } else {
-    //                     const vr = JSON.stringify(response, null, 2)
-    //                     const classes = response.result.images[0].classifiers[0].classes;
-    //                     if (classes[0]=='explicit'){
-    //                         console.log('0') //No aprobada
-    //                     } else{
-    //                         VisualRecognition.classify(VisualClassifyParams_General, function (err,response){
-    //                             if (err){
-    //                                 console.log(err);
-    //                             } else {
-    //                                 const vr2 = JSON.stringify(response, null, 2)
-    //                                 const classes2 = response.result.images[0].classifiers[0].classes;
-    //                                 console.log(classes2)
-    //                             };
-
-    //                         });
-    //                     }
-    //                     //console.log(classes)
-    //                 }
-    //             });
-    //         })
-    //     });
-    // } else if (files[files.length-1].slice(0,5)=='texto'){
-    //     const classifyParamsText = {
-    //         text: 'Me gusta la cuk',
-    //         classifierId: parameters.LC_MODEL_ID
-    //     };
-
-    //     NaturalLanguageCLassifier.classify(classifyParamsText, function (err,response){
-    //         if(err){
-    //             console.log(err);
-    //         } else{
-    //             const lc = JSON.stringify(response, null, 2)
-    //             const classesLC = response.result.classes;
-    //             console.log(classesLC)
-    //         };
-
-    //     });
-    // };
-
-     });
-    //console.log(url);
-}
-bucket();
-
-
-
-
-
-
-// const functions = require("firebase-functions");
-// exports.ImageClassification = functions.storage.object().onFinalize((event) => {
-//     const url = event.url;
+//     await bucket.file(path).createReadStream()
+//     .on('data', (data) => {
+//         console.log(data)
+//     })
 // });
-// object.name.startsWith('cover_streamings/')
+
+const x = async(bucket2, filePath) => {
+    const bucket = storage.bucket(bucket2);
+    const fileName = filePath.split('/').pop();
+    const bucketDir = path.dirname(filePath);
+    const localFilename = './image.jpeg'
+    const stream = await bucket.file(filePath).createReadStream().on('response', response => {
+        console.log(response);
+    }).on('end', () => {
+        console.log('Here');
+    })
+    .pipe(fs.createWriteStream(localFilename));
+}
+x('ibm-challenge-d1eaa.appspot.com','profile/yvqingvusrc_200x200.jpeg');
 
 
